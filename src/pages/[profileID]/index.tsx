1244e49977fe41f03bc9960c3c2dd39fcfb012b4
subrat gupta <guptasubratkumar@gmail.com>
2022-11-19 16:35:21 +0530

    adde 3x3 list ui to profile

--- src/pages/[profileID]/index.tsx
+++ src/pages/[profileID]/index.tsx
@@ -4,7 +4,394 @@ import { useRouter } from "next/router";
 import { trpc } from "../../utils/trpc";
 import React from "react";
 import { UserProfile } from "../../components/social/profilePage/userInfo";
-import Costom404 from "../404";
+
+const posts = [
+  {
+    id: "random",
+    name: "my_post_3by3_1",
+    comment: [
+      {
+        comenter: "subrat",
+        content: "i like this",
+      },
+      {
+        comenter: "subrat",
+        content: "i like this",
+      },
+    ],
+  },
+  {
+    id: "random",
+    name: "my_post_3by3_1",
+    comment: [
+      {
+        comenter: "subrat",
+        content: "i like this",
+      },
+      {
+        comenter: "subrat",
+        content: "i like this",
+      },
+    ],
+  },
+  {
+    id: "random",
+    name: "my_post_3by3_1",
+    comment: [
+      {
+        comenter: "subrat",
+        content: "i like this",
+      },
+      {
+        comenter: "subrat",
+        content: "i like this",
+      },
+    ],
+  },
+  {
+    id: "random",
+    name: "my_post_3by3_1",
+    comment: [
+      {
+        comenter: "subrat",
+        content: "i like this",
+      },
+      {
+        comenter: "subrat",
+        content: "i like this",
+      },
+    ],
+  },
+  {
+    id: "random",
+    name: "my_post_3by3_1",
+    comment: [
+      {
+        comenter: "subrat",
+        content: "i like this",
+      },
+      {
+        comenter: "subrat",
+        content: "i like this",
+      },
+    ],
+  },
+  {
+    id: "random",
+    name: "my_post_3by3_1",
+    comment: [
+      {
+        comenter: "subrat",
+        content: "i like this",
+      },
+      {
+        comenter: "subrat",
+        content: "i like this",
+      },
+    ],
+  },
+  {
+    id: "random",
+    name: "my_post_3by3_1",
+    comment: [
+      {
+        comenter: "subrat",
+        content: "i like this",
+      },
+      {
+        comenter: "subrat",
+        content: "i like this",
+      },
+    ],
+  },
+  {
+    id: "random",
+    name: "my_post_3by3_1",
+    comment: [
+      {
+        comenter: "subrat",
+        content: "i like this",
+      },
+      {
+        comenter: "subrat",
+        content: "i like this",
+      },
+    ],
+  },
+  {
+    id: "random",
+    name: "my_post_3by3_1",
+    comment: [
+      {
+        comenter: "subrat",
+        content: "i like this",
+      },
+      {
+        comenter: "subrat",
+        content: "i like this",
+      },
+    ],
+  },
+  {
+    id: "random",
+    name: "my_post_3by3_1",
+    comment: [
+      {
+        comenter: "subrat",
+        content: "i like this",
+      },
+      {
+        comenter: "subrat",
+        content: "i like this",
+      },
+    ],
+  },
+  {
+    id: "random",
+    name: "my_post_3by3_1",
+    comment: [
+      {
+        comenter: "subrat",
+        content: "i like this",
+      },
+      {
+        comenter: "subrat",
+        content: "i like this",
+      },
+    ],
+  },
+  {
+    id: "random",
+    name: "my_post_3by3_1",
+    comment: [
+      {
+        comenter: "subrat",
+        content: "i like this",
+      },
+      {
+        comenter: "subrat",
+        content: "i like this",
+      },
+    ],
+  },
+  {
+    id: "random",
+    name: "my_post_3by3_1",
+    comment: [
+      {
+        comenter: "subrat",
+        content: "i like this",
+      },
+      {
+        comenter: "subrat",
+        content: "i like this",
+      },
+    ],
+  },
+  {
+    id: "random",
+    name: "my_post_3by3_1",
+    comment: [
+      {
+        comenter: "subrat",
+        content: "i like this",
+      },
+      {
+        comenter: "subrat",
+        content: "i like this",
+      },
+    ],
+  },
+  {
+    id: "random",
+    name: "my_post_3by3_1",
+    comment: [
+      {
+        comenter: "subrat",
+        content: "i like this",
+      },
+      {
+        comenter: "subrat",
+        content: "i like this",
+      },
+    ],
+  },
+  {
+    id: "random",
+    name: "my_post_3by3_1",
+    comment: [
+      {
+        comenter: "subrat",
+        content: "i like this",
+      },
+      {
+        comenter: "subrat",
+        content: "i like this",
+      },
+    ],
+  },
+  {
+    id: "random",
+    name: "my_post_3by3_1",
+    comment: [
+      {
+        comenter: "subrat",
+        content: "i like this",
+      },
+      {
+        comenter: "subrat",
+        content: "i like this",
+      },
+    ],
+  },
+  {
+    id: "random",
+    name: "my_post_3by3_2",
+    comment: [
+      {
+        comenter: "subrat",
+        content: "i like this",
+      },
+      {
+        comenter: "subrat",
+        content: "i like this",
+      },
+    ],
+  },
+  {
+    id: "random",
+    name: "my__3by3_3",
+    comment: [
+      {
+        comenter: "subrat",
+        content: "i like this",
+      },
+      {
+        comenter: "subrat",
+        content: "i like this",
+      },
+    ],
+  },
+  {
+    id: "random",
+    name: "_4",
+    comment: [
+      {
+        comenter: "subrat",
+        content: "i like this",
+      },
+      {
+        comenter: "subrat",
+        content: "i like this",
+      },
+    ],
+  },
+  {
+    id: "random",
+    name: "my_post_dddssssssssd3by3_5",
+    comment: [
+      {
+        comenter: "subrat",
+        content: "i like this",
+      },
+      {
+        comenter: "subrat",
+        content: "i like this",
+      },
+    ],
+  },
+];
+type TxT = {
+  posts: typeof posts;
+  setAcative: () => void;
+};
+const TxT = ({}: TxT) => {
+  return (
+    <>
+      {posts.map((post) => {
+        return (
+          <>
+            <div className="mx-auto mt-3 mb-3">
+              <div className="max-w-sm rounded-lg border border-gray-200 bg-white shadow-md dark:border-gray-700 dark:bg-gray-800">
+                <div className=" h-96 w-96"></div>
+                <div className="p-5">
+                  <a href="#">
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                      {post.name}
+                    </h5>
+                  </a>
+                  <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                    {`"Here are the biggest enterprise technology acquisitions of2021 so far," ${post.name}`}
+                  </p>
+                  <a
+                    href="#"
+                    className="inline-flex items-center rounded-lg bg-blue-700 px-3 py-2 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  >
+                    Read more
+                    <svg
+                      aria-hidden="true"
+                      className="ml-2 -mr-1 h-4 w-4"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                        clip-rule="evenodd"
+                      ></path>
+                    </svg>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </>
+        );
+      })}
+    </>
+  );
+};
+type T3TListType = {
+  acative: string;
+  posts: typeof posts;
+};
+const T3TList = ({ acative, posts }: T3TListType) => {
+  return (
+    <>
+      <div className="relative flex h-full w-full justify-end  overflow-x-clip overflow-y-scroll bg-[#0f172a]  scrollbar-hide">
+        <div className="pointer-events-none fixed h-full w-full shadow-t shadow-[#0f172a]"></div>
+        <div className=" h-full w-full">
+          <div className="3by3 h-7  pr-4 ">
+            <span className="inline w-full cursor-pointer overflow-clip overflow-ellipsis whitespace-nowrap pl-24 text-xl font-semibold text-gray-400 hover:text-gray-300 active:text-blue-700"></span>
+          </div>
+          {posts.map((post, i) => {
+            return (
+              <div key={i} className="3by3 h-12  pr-4 ">
+                <span
+                  className=" 
+                        inline 
+                        w-full 
+                        cursor-pointer 
+                        overflow-clip
+                        overflow-ellipsis  
+                        whitespace-nowrap 
+                        pl-24 
+                        text-xl 
+                        font-semibold 
+                      text-gray-400 
+                      hover:text-gray-300 
+                      active:text-blue-700"
+                >
+                  {`--${post.name}`}
+                </span>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+};
 
 const ProfilePage: NextPage = () => {
   const router = useRouter();
@@ -24,7 +411,8 @@ const ProfilePage: NextPage = () => {
 
   if (isLoading) return <div>loding...</div>;
   if (error) {
-    Costom404;
+    error.data;
+    router.push("/404");
   }
   function capitalizeFirstLetter(string: string | undefined | null) {
     return `${string?.charAt(0).toUpperCase()}` + `${string?.slice(1)}`;
@@ -35,6 +423,7 @@ const ProfilePage: NextPage = () => {
       ? data?.profile.getUser?.image
       : "https://www.freeiconspng.com/img/23486"
   }`;
+
   if (data)
     return (
       <>
@@ -43,7 +432,6 @@ const ProfilePage: NextPage = () => {
           <meta name="3x3" content="create a 3x3" />
           <link rel="icon" href="/favicon.ico" />
         </Head>
-
         <main
           className="
           grid   
@@ -55,9 +443,17 @@ const ProfilePage: NextPage = () => {
         >
           <div className="flex h-full w-full flex-col  bg-slate-500">
             <UserProfile type={"NOT_FOLLOWING"} image={image} name={Username} />
-            <div className="h-full w-full bg-orange-500"> joojo</div>
+            <T3TList acative={"po"} posts={posts} />
           </div>
-          <div className="h-full w-full bg-slate-600"></div>
+          <div className="flex h-full w-full flex-col overflow-y-scroll bg-slate-600 scrollbar-hide">
+            {/* temp post for testing */}
+            <TxT
+              posts={posts}
+              setAcative={function (): void {
+                throw new Error("Function not implemented.");
+              }}
+            />
+          </div>
           <div className="h-full w-full  bg-slate-700"></div>
         </main>
       </>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,9 @@ module.exports = {
       backgroundImage: {
         "dark-gradint-1": "url('/img/dark_backround.svg')",
       },
+      boxShadow: {
+        t: `inset 0px 13px 10px 7px `,
+      },
     },
   },
   plugins: [require("tailwind-scrollbar-hide")],
