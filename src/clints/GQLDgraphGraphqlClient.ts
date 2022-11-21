48a6b2c9dc65b38d27d76cd9312cd641e0050c00
subrat gupta <guptasubratkumar@gmail.com>
2022-11-21 21:08:34 +0530

    fixed degraph endpoint

--- src/clints/GQLDgraphGraphqlClient.ts
+++ src/clints/GQLDgraphGraphqlClient.ts
@@ -2,7 +2,7 @@ import { GraphQLClient } from "graphql-request";
 import { env } from "../env/server.mjs";
 
 export const DgraphGraphqlRequestClient = new GraphQLClient(
-  "http://localhost:8080/graphql",
+  env.DGRAPH_GRAPHQL_ENDPOINT,
   {
     method: "POST",
     headers: {
