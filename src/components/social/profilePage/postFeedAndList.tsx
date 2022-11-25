ad1c0cd0013a744f4e2695ec15af47b8781d7a54
subrat gupta <guptasubratkumar@gmail.com>
2022-11-25 18:44:25 +0530

    fixed reply ui state

--- src/components/social/profilePage/postFeedAndList.tsx
+++ src/components/social/profilePage/postFeedAndList.tsx
@@ -1,6 +1,7 @@
 import { useRouter } from "next/router";
 import { useRef, useEffect } from "react";
 import { createRefWithDefualtValue } from "../../utils/CreateRefDefult";
+import { Post3x3 } from "../posts/3x3";
 import { PostType } from "./profile";
 
 //store the state for if postfeed is being auto scrolled or not
@@ -65,33 +66,7 @@ export const TxT = ({ posts, acative }: TxT) => {
               key={i}
               className="mx-auto mt-3 mb-3 "
             >
-              <div className="max-w-sm rounded-lg border border-gray-200 bg-white shadow-md dark:border-gray-700 dark:bg-gray-800">
-                <div className=" h-96 w-96 cursor-pointer bg-[#0f172a]"></div>
-                <div className="p-5">
-                  <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                    {post.name}
-                  </h5>
-                  <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                    {`"Here are the biggest enterprise technology acquisitions of2021 so far," ${post.name}`}
-                  </p>
-                  <div className="inline-flex items-center rounded-lg bg-blue-700 px-3 py-2 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                    Read more
-                    <svg
-                      aria-hidden="true"
-                      className="ml-2 -mr-1 h-4 w-4"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                        clipRule="evenodd"
-                      ></path>
-                    </svg>
-                  </div>
-                </div>
-              </div>
+              <Post3x3 name={post.name} />
             </div>
           );
         })}
